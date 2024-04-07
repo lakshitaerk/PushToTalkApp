28a8e0bb71d7b3aabeb2a289f35848e69919bff5
Lakshita <lakshita2119@gmail.com>
2024-04-08 00:06:44 +0530

    Initial Commit

---
+++ PushToTalkApp/PushToTalkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushToTalkAppApp.swift
+//  PushToTalkApp
+//
+//  Created by lakshita sodhi on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PushToTalkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

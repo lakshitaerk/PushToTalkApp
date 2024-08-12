f58d15e5f8fab0de0b2d0e288faf819df8d41bf2
Lakshita <lakshita2119@gmail.com>
2024-08-12 21:18:23 +0530

    scratch card view

---
+++ PushToTalkApp/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  PushToTalkApp
+//
+//  Created by lakshita sodhi on 08/08/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
